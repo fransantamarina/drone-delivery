93dec236c6a40b45844495d648b4d8886b76a0f0
Francisco Santamarina <65471703+fransantamarina@users.noreply.github.com>
2023-03-25 01:51:34 -0300

    Delete init.sql

--- init.sql
+++
@@ -1,3 +0,0 @@
-CREATE SCHEMA drone_delivery;
-
-INSERT INTO drone_delivery.drone (id, serial_number, model, weight_limit, battery_capacity, state) VALUES ('18', 'PANCHO123', 'CRUISERWEIGHT', '500.00', '100', 'IDLE');
